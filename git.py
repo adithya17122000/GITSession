fcaac900fc69ae4b76abab3cdb7b9767678a2831
adithya17122000 <adithya.anjanappa@Triconinfotech.com>
2024-11-05 12:43:53 +0530

    Update git.py

--- git.py
+++ git.py
@@ -1,9 +1,11 @@
-print('Hello GIT')
+# main.py
+def add_numbers(a, b):
+    return a + b
 
-a= 1
-
-b=2
-
-c=a+b
-print("bye~!")
-print(c)
+if __name__ == "__main__":
+    print("Hello GIT")
+    a = 1
+    b = 2
+    c = add_numbers(a, b)
+    print("bye~!")
+    print(c)
